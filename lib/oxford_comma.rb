72a128fad0812c53f46d3aab5f588f09a9247185
Michael Harris <xero5k@yahoo.com>
2022-02-04 20:53:34 -0600

    completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  #binding.pry
+  joined_array = []
+  if array.size == 1
+    return array.join()
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size >= 3
+    last_word = array.pop
+    joined_array = array.join(", ")
+    joined_array + ", and " + last_word
+  end 
 end
+
+oxford_comma(["bananas", "monkeys"])
+oxford_comma(["bananas", "monkeys", "biscuits", "pants"])
